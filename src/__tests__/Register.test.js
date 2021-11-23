c7388b99c62447826d403532901efddd6601f71b
Nabhan <nabhan@criodo.com>
2021-11-24 02:54:48 +0550

    updated changes - 2021-11-24 02:34:48

--- src/__tests__/Register.test.js
+++ src/__tests__/Register.test.js
@@ -16,7 +16,7 @@ mock
     username: "crio.do",
     password: "learnbydoing",
   })
-  .reply(200, { success: true });
+  .reply(201, { success: true });
 
 mock
   .onPost(`${config.endpoint}/auth/register`, {
