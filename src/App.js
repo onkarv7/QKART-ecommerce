6e71138721ae13311ba5aa989f7c0f12c87184be
Nabhan Abdulla P V <nabhan@criodo.com>
2021-11-27 05:22:21 +0000

    Update src/App.js Deleted src/components/Cart.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Register from "./components/Register";
 import ipConfig from "./ipConfig.json";
 
 export const config = {
-  // endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
-  endpoint: `https://qkart-frontend.herokuapp.com/api/v1`,
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
 };
 
 function App() {
